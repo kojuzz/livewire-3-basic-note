2c400380f3058b466a3f6d7e3d8830dc0dd5cf21
Ko Juzz <sawdani@gmail.com>
2025-05-16 13:14:42 +0700

    [🚧 9] Form Create and Save

--- app/Livewire/CreateBook.php
+++ app/Livewire/CreateBook.php
@@ -2,11 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Book;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 class CreateBook extends Component
 {
     // #[Layout('components.layouts.second')]
+    public $title;
+    public $author;
+    public $rating;
+
+    public function save()
+    {
+        Book::create([
+            'title' => $this->title,
+            'author' => $this->author,
+            'rating' => $this->rating
+        ]);
+        $this->redirect('/');
+    }
+
     public function render()
     {
         return view('livewire.create-book');
--- resources/views/livewire/create-book.blade.php
+++ resources/views/livewire/create-book.blade.php
@@ -1,3 +1,18 @@
 <div class="create">
     <h3>Create new book</h3>
+    <form wire:submit="save">
+        <div class="field">
+            <label for="">Book Title:</label>
+            <input type="text" wire:model="title">
+        </div>
+        <div class="field">
+            <label for="">Book Author:</label>
+            <input type="text" wire:model="author">
+        </div>
+        <div class="field">
+            <label for="">Book Rating:</label>
+            <input type="number" wire:model="rating">
+        </div>
+        <button>Add Book</button>
+    </form>
 </div>
