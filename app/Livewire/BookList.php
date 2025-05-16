bc2975b5a604ad9fb7c22f008baec851451d75ee
Ko Juzz <sawdani@gmail.com>
2025-05-16 11:32:14 +0700

    [🚧 2] - Foreach with key

---
+++ app/Livewire/BookList.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Book;
+use Livewire\Component;
+
+class BookList extends Component
+{
+    public $name = "Mario";
+    public function render()
+    {
+        return view('livewire.book-list', [
+            'books' => Book::all()
+        ]);
+    }
+}
