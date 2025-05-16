87c428835754f93d57c33caefe514323766ae1a7
Ko Juzz <sawdani@gmail.com>
2025-05-16 13:22:09 +0700

    [🚧 10] Validation rules (two methods) and showing errors

--- app/Livewire/CreateBook.php
+++ app/Livewire/CreateBook.php
@@ -4,16 +4,25 @@ namespace App\Livewire;
 
 use App\Models\Book;
 use Livewire\Component;
-use Livewire\Attributes\Layout;
+// use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
+
 class CreateBook extends Component
 {
     // #[Layout('components.layouts.second')]
+    #[Rule('required|string|min:3|max:50')]
     public $title;
+
+    #[Rule('required|string|min:3|max:50')]
     public $author;
+
+    #[Rule('required|integer|min:1|max:10')]
     public $rating;
 
     public function save()
     {
+        $this->validate();
+        
         Book::create([
             'title' => $this->title,
             'author' => $this->author,
--- resources/views/livewire/create-book.blade.php
+++ resources/views/livewire/create-book.blade.php
@@ -4,14 +4,23 @@
         <div class="field">
             <label for="">Book Title:</label>
             <input type="text" wire:model="title">
+            @error('title')
+                <span class="error">{{ $message }}</span>
+            @enderror
         </div>
         <div class="field">
             <label for="">Book Author:</label>
             <input type="text" wire:model="author">
+            @error('author')
+                <span class="error">{{ $message }}</span>
+            @enderror
         </div>
         <div class="field">
             <label for="">Book Rating:</label>
             <input type="number" wire:model="rating">
+            @error('rating')
+                <span class="error">{{ $message }}</span>
+            @enderror
         </div>
         <button>Add Book</button>
     </form>
